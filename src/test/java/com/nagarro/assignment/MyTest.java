c04fb6ee26f962715bc70c941d5e2abcde8f815d
NAGARRO\sakshamgupta <saksham.gupta@nagarro.com>
2018-03-15 14:38:45 +0530

    Project added

---
+++ src/test/java/com/nagarro/assignment/MyTest.java
@@ -0,0 +1,17 @@
+package com.nagarro.assignment;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class MyTest {
+	
+	@Test
+	public void test(){
+		assertEquals(1,1);
+	}
+
+}
